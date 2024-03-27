2983328ad8efec17a216f8a840dd0a2c064e6242
phonggp <phong.giang@navigosgroup.com>
2024-03-27 15:05:32 +0700

    test module

---
+++ test_module_go.go
@@ -0,0 +1,23 @@
+package test_module_go
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func ArrayIntToString(input []int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(input), " ", delim, -1), "[]")
+}
+
+func StringToIntArray(s string) []int {
+	if s == "" {
+		return make([]int, 0)
+	}
+	arr := strings.Split(s, ",")
+	b := make([]int, len(arr))
+	for i, v := range arr {
+		b[i], _ = strconv.Atoi(v)
+	}
+	return b
+}
